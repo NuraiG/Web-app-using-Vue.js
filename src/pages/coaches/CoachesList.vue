7751f7a86e7b3e07aa2fbae91f532c36b0f5c362
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-06-28 14:40:26 +0300

    Added base components

--- src/pages/coaches/CoachesList.vue
+++ src/pages/coaches/CoachesList.vue
@@ -1,25 +1,52 @@
 <template>
   <section>Filter</section>
   <section>
+    <base-card>
     <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as a coach</router-link>
+      <base-button mode="outline">Refresh</base-button>
+      <base-button link to="/register">Register as a coach</base-button>
     </div>
     <ul v-if="hasCoaches">
-      <li v-for="coach in filteredCoaches" :key="coach.id">{{ coach.firstName}}</li>
+      <coach-item
+        v-for="coach in filteredCoaches"
+        :key="coach.id"
+        :id="coach.id"
+        :first-name="coach.firstName"
+        :last-name="coach.lastName"
+        :rate="coach.hourlyRate"
+        :areas="coach.areas"
+        >{{ coach.firstName }}</coach-item
+      >
     </ul>
     <h3 v-else>No coaches found.</h3>
+    </base-card>
   </section>
 </template>
 <script>
+import CoachItem from "../../components/coaches/CoachItem.vue";
+import BaseButton from '../../components/ui/BaseButton.vue';
 export default {
-  computed:{
-    filteredCoaches(){
-      return this.$store.getters['coaches/coaches'];
+  components: { CoachItem, BaseButton },
+  computed: {
+    filteredCoaches() {
+      return this.$store.getters["coaches/coaches"];
     },
-    hasCoaches(){
-      return this.$store.getters['coaches/hasCoaches'];
-    }
-  }
-}
+    hasCoaches() {
+      return this.$store.getters["coaches/hasCoaches"];
+    },
+  },
+};
 </script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
