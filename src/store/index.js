a02ae5bc5e59473062d64280a4010be229c47458
Nuray Gulistan <nuray.b.gulistan@gmail.com>
2021-06-29 11:19:36 +0300

    Аdded save coach functionality

--- src/store/index.js
+++ src/store/index.js
@@ -2,9 +2,19 @@ import { createStore } from "vuex";
 import coachesModules from "./modules/coaches/index.js";
 
 const store = createStore({
-    modules:{
-        coaches: coachesModules,
-    }
+  modules: {
+    coaches: coachesModules,
+  },
+  state() {
+    return {
+      userId: "c3",
+    };
+  },
+  getters:{
+     userId(state){
+         return state.userId;
+     }
+  },
 });
 
 export default store;
--- src/store/modules/coaches/getters.js
+++ src/store/modules/coaches/getters.js
@@ -5,4 +5,9 @@ export default {
   hasCoaches(state) {
     return state.coaches && state.coaches.length;
   },
+  isCoach(_1, getters, _2, rootGetters) {
+    const coaches = getters.coaches;
+    const userId = rootGetters.userId;
+    return coaches.some((el) => el.id === userId);
+  },
 };
